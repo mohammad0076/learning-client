9cfff8a9f8cf23cfbad6eadff84d0b0fbc69963c
mohammad0076 <mohammadullahpatwary.com@gmail.com>
2022-10-27 18:05:26 +0600

    blog

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
-            <h1> i am blogs</h1>
+        <div className='ms-5 mt-5'>
+            <h1> what is cors?</h1>
+            <p>cors is  method that allows you to make requests to the server deployed at a different domain</p>
+            <h1>what is firebase?</h1>
+            <p>Firebase allows  to track your users' journey through realtime and custom reporting.</p>
+            <p>other options areAuth0, MongoDB, Passport, Okta,</p>
+            <h1>How does the private route work?</h1>
+            <p>renders child components ( children ) if the user is logged in</p>
+            <h1>what is node js?how does it work?</h1>
+            <p>It is used for server-side programming.Node. js runs on chrome v8 engine which converts javascript code into machine code</p>
         </div>
     );
 };
